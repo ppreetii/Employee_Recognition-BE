ee5182ad7ed41999a10de8b329c86eb3f5d7e6cf
ppreetii <ppreeti.guptaa@gmail.com>
2023-04-17 02:19:07 +0530

    Employee of the Month Implementation
    Fixed Bug with EmpOfTheDay
    
    small refatoring in empOfTheMonth

--- src/services/reward.js
+++ src/services/reward.js
@@ -1,8 +1,11 @@
+const sequelize = require("sequelize");
+
 const EmpOfTheDayModel = require("../models/employee_of_the_day");
 const COMMON = require("../constants/common");
 const Utils = require("../utils/helper");
 const holidayList = require("../utils/data/holidays");
 const Employee = require("../models/employee");
+const Task = require("../models/task");
 
 exports.getEmployees = async (rewardType, date) => {
   try {
@@ -59,7 +62,6 @@ let getEmployeeOfTheDay = async (date) => {
 
       // Check if it is 7 PM
       if (currentHour < 19) {
-      
         let lastDate = Utils.findLastWeekDay(date);
         data = await EmpOfTheDayModel.getEmployeeOfTheDay(lastDate);
       }
@@ -69,23 +71,22 @@ let getEmployeeOfTheDay = async (date) => {
 
         if (Array.isArray(data) && data.length > 0) {
           const employee = await Employee.findOne({
-            where:{
-              id: data[0].employeeId
-            }
+            where: {
+              id: data[0].employeeId,
+            },
           });
 
-          if(!employee){
-            throw new Error('Employee Not Found')
+          if (!employee) {
+            throw new Error("Employee Not Found");
           }
 
-          if(employee.bonusStars + 50 < 5000){
-            employee.bonusStars += 50
+          if (employee.bonusStars + 50 < 5000) {
+            employee.bonusStars += 50;
           }
 
           employee.employee_of_the_day += 1;
-          
+
           await employee.save();
-          
         }
       }
 
@@ -114,6 +115,41 @@ let getEmployeeOfTheDay = async (date) => {
 
 let getEmployeeOfTheWeek = async (date) => {
   try {
+    if (Utils.isFutureDate(date)) {
+      throw new Error("Date is in the future.");
+    }
+
+    let { lastMonday, nextMonday } = Utils.getMondays(new Date(date));
+    
+    if (new Date() < nextMonday) {
+      return {
+        message: "Results will be declared on Monday.",
+      };
+    }
+
+    let employee = await Task.findAndCountAll({
+      attributes: [
+        [sequelize.fn("COUNT", sequelize.col("employeeId")), "totalTasks"],
+      ],
+      where: {
+        status: COMMON.TASK_STATUS.DONE,
+        date_completed: {
+          [sequelize.Op.between]: [lastMonday, nextMonday],
+        },
+      },
+      include: {
+        model: Employee,
+        attributes: ["id", "name", "designation"],
+      },
+      order: [["totalTasks", "DESC"]],
+      limit: 1,
+    });
+
+    return {
+      ...employee?.rows[0]?.employee.dataValues,
+      startOfWeek: lastMonday,
+      endOfWeek: nextMonday,
+    };
   } catch (error) {
     throw error;
   }
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -41,3 +41,49 @@ exports.findLastWeekDay = (date) => {
 
   return lastDate;
 };
+
+exports.daysDifference = (date_1, date_2) => {
+  let difference = date_2.getTime() - date_1.getTime();
+  let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+
+  return TotalDays;
+};
+
+exports.getMondays = (date) => {
+  const daysUntilMonday =
+    date.getDay() === 0 ? 6 : date.getDay() - 1 === 0 ? 7 : date.getDay() - 1;
+
+  const lastMonday = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate() - daysUntilMonday
+  );
+
+  const nextMonday =
+    date.getDay() !== 1
+      ? new Date(
+          lastMonday.getFullYear(),
+          lastMonday.getMonth(),
+          lastMonday.getDate() + 7
+        )
+      : date;
+
+  const formatDate = (dateString) => {
+    const dateParts = dateString.split(/[/: ,]/);
+    const year = parseInt(dateParts[2]);
+    const month = parseInt(dateParts[1]);
+    const day = parseInt(dateParts[0]);
+
+    const monthString = month.toString().padStart(2, "0"); // Pad month component with leading zero if necessary
+    const formattedDate = `${year}-${monthString}-${day
+      .toString()
+      .padStart(2, "0")}`;
+
+    return formattedDate;
+  };
+
+  return {
+    lastMonday: formatDate(lastMonday.toLocaleString()),
+    nextMonday: formatDate(nextMonday.toLocaleString()),
+  };
+};
